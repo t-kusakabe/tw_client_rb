3b377c66b248a3288af92bc4fbacb700b2e4e3a6
t-kusakabe <ttmac0704@gmail.com>
2019-01-10 12:21:32 +0900

    shunさんの情報を返す :heart:

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,8 @@
+require 'sinatra'
+require 'sinatra/reloader'
 require 'dotenv'
 require 'twitter'
+require 'pry'
 
 Dotenv.load
 
@@ -8,4 +11,10 @@ client = Twitter::REST::Client.new do |config|
   config.consumer_secret     = ENV['CONSUMER_SECRET']
   config.access_token        = ENV['ACCESS_TOKEN']
   config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+user = client.user('shunn_93')
+
+get '/' do
+  p user.attrs.to_json
 end
